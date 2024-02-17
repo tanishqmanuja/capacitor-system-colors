88685517b4401e969c42613ecd8ed95a9ca5ed3f
tanishqmanuja <tanishqmanuja@gmail.com>
2024-02-17 21:31:29 +0530

    feat: update definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,4 @@
 export interface SystemColorsPlugin {
+  /** @description Is equivalent to calling getColor(context, android.R.color.{id}) on android. */
   get(options: { id: string }): Promise<{ color: string }>;
 }
